3477ca6a342e9a061490ef7253580848cfff1732
Sanjaya Rai <sonjrai@gmail.com>
2026-02-22 23:59:57 +0545

    responsiveness fix on music player

--- src/components/MusicPlayer/MusicControls/MusicControls.tsx
+++ src/components/MusicPlayer/MusicControls/MusicControls.tsx
@@ -1,3 +1,4 @@
+import MusicTimer from "@/components/MusicTimer/MusicTimer";
 import { MusicControlsProps } from "@/interface/Music";
 import {
   Pause,
@@ -11,58 +12,69 @@ import {
 const MusicControls = ({
   fromMobilePlayer = false,
   isPlaying,
+  currentTime,
+  duration,
   onPlayPause,
 }: MusicControlsProps) => {
   return (
-    <div
-      data-id="music-controls"
-      className=" flex flex-row gap-4 z-10 items-center justify-end bg-transparent"
-    >
-      {!fromMobilePlayer && (
-        <Shuffle
-          size={20}
-          className="text-[#ffffff80] hover:text-white cursor-pointer"
-        />
-      )}
-      <SkipBack
-        size={20}
-        className="text-[#ffffff80] hover:text-white cursor-pointer max-[342px]:hidden"
-      />
+    <>
       <div
-        className={`flex items-center rounded-full
+        data-id="music-controls"
+        className=" flex flex-row gap-4 z-10 items-center justify-end bg-transparent"
+      >
+        {fromMobilePlayer && (
+          <MusicTimer
+            currentTime={currentTime}
+            duration={duration}
+            className="max-[430px]:hidden"
+          />
+        )}
+        {!fromMobilePlayer && (
+          <Shuffle
+            size={20}
+            className="text-[#ffffff80] hover:text-white cursor-pointer"
+          />
+        )}
+        <SkipBack
+          size={20}
+          className="text-[#ffffff80] hover:text-white cursor-pointer max-[342px]:hidden"
+        />
+        <div
+          className={`flex items-center rounded-full
           ${
             fromMobilePlayer
               ? "p-2  bg-[#ffffff1a] border-0 hover:bg-[#ffffff26]"
-              : `h-[42px] w-[42px] justify-center border border-[#ffffff]/5`
+              : `h-10.5 w-10.5 justify-center border border-[#ffffff]/5`
           }`}
-        onClick={(e) => {
-          e.stopPropagation();
-          onPlayPause();
-        }}
-      >
-        {isPlaying ? (
-          <Pause
-            size={24}
-            className=" flex items-center text-[#ffffff80] hover:text-white cursor-pointer"
-          />
-        ) : (
-          <Play
-            size={24}
-            className=" flex items-center text-[#ffffff80] hover:text-white cursor-pointer"
+          onClick={(e) => {
+            e.stopPropagation();
+            onPlayPause();
+          }}
+        >
+          {isPlaying ? (
+            <Pause
+              size={24}
+              className=" flex items-center text-[#ffffff80] hover:text-white cursor-pointer"
+            />
+          ) : (
+            <Play
+              size={24}
+              className=" flex items-center text-[#ffffff80] hover:text-white cursor-pointer"
+            />
+          )}
+        </div>
+        <SkipForward
+          size={20}
+          className="text-[#ffffff80] hover:text-white cursor-pointer max-[342px]:hidden"
+        />
+        {!fromMobilePlayer && (
+          <Repeat
+            size={20}
+            className="text-[#ffffff80] hover:text-white cursor-pointer"
           />
         )}
       </div>
-      <SkipForward
-        size={20}
-        className="text-[#ffffff80] hover:text-white cursor-pointer max-[342px]:hidden"
-      />
-      {!fromMobilePlayer && (
-        <Repeat
-          size={20}
-          className="text-[#ffffff80] hover:text-white cursor-pointer"
-        />
-      )}
-    </div>
+    </>
   );
 };
 
