9451d099896c298b12c67fb929c75d521ae7c927
Sanjay Rai <43443174+Sanjay4715@users.noreply.github.com>
2026-02-22 20:05:32 +0545

    merge develop to master (#24)
    * Merge pull request #16 from Sanjay4715/develop (#17)
    
    * get generated lists (#18)
    
    * Merging develop to master (#19) (#20)
    
    * Merge pull request #16 from Sanjay4715/develop (#17)
    
    * get generated lists (#18)
    
    * web socket implemented, dynamic processing generation items (#21)
    
    * configure socket on separate repo

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,8 +1,10 @@
 import { io, Socket } from "socket.io-client";
 
+console.log(process.env.NODE_ENV);
+
 const SOCKET_URL =
   process.env.NODE_ENV === "production"
-    ? "https://musicgpt-assessment.vercel.app"
-    : "http://localhost:4000";
+    ? "https://musicgpt-assessment-socket-production.up.railway.app"
+    : "http://localhost:4000/";
 
 export const socket: Socket = io(SOCKET_URL, { autoConnect: true });
