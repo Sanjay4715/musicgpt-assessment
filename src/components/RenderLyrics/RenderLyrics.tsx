664e80e67ff1aa0666c0b48e2f6f940e12bea6fe
Sanjaya Rai <sonjrai@gmail.com>
2026-02-23 15:21:19 +0545

    lyrics rendering

--- src/components/RenderLyrics/RenderLyrics.tsx
+++ src/components/RenderLyrics/RenderLyrics.tsx
@@ -1,46 +1,33 @@
-import { useMusicPlayerStore } from "@/store/useMusicPlayerStore";
-import { useEffect, useState } from "react";
+import { RenderLyricsProps } from "@/interface/MusicPlayer";
 
-interface Lyrics {
-  start: number;
-  end: number;
-  text: string;
-  index: number;
-}
+const RenderLyrics = ({ lyrics }: RenderLyricsProps) => {
+  // Split by section tags: [Verse 1], [Chorus], etc.
+  const parts = lyrics.split(/(\[.*?\])/g); // keep the brackets
 
-const RenderLyrics = ({ lyrics }: { lyrics: string }) => {
-  const { currentTimestamp, musicToPlay } = useMusicPlayerStore();
-  const lyricsArray: Lyrics[] = JSON.parse(
-    musicToPlay?.lyrics_output_timestamped ?? "[]",
-  );
-
-  const [currentLine, setCurrentLine] = useState<Lyrics>();
-
-  useEffect(() => {
-    const currentMs = currentTimestamp * 1000; // convert seconds → ms
-    if (musicToPlay?.lyrics_output_timestamped) {
-      // Parse the string into an array
-
-      if (lyricsArray.length > 0) {
-        const line = lyricsArray.find(
-          (l: Lyrics) => currentMs >= l.start && currentMs <= l.end,
+  return (
+    <div>
+      {parts.map((part, index) => {
+        if (part.match(/\[.*?\]/)) {
+          return (
+            <span
+              key={index}
+              className="font-bold text-[#ffffff80] text-[16px]"
+            >
+              {part}
+            </span>
+          );
+        }
+        return (
+          <span
+            key={index}
+            className="whitespace-pre-line text-[#ffffff80] leading-6"
+          >
+            {part}
+          </span>
         );
-
-        setCurrentLine(line); // update state
-      }
-    }
-  }, [currentTimestamp, musicToPlay]);
-
-  if (!lyrics) return null;
-
-  return lyricsArray.map((lryic) => (
-    <div
-      key={lryic.index}
-      className={`whitespace-pre-line leading-6 ${lryic.text === currentLine?.text ? "text-[#ffffff]" : "text-[#ffffff80]"}`}
-    >
-      {lryic.text}
+      })}
     </div>
-  ));
+  );
 };
 
 export default RenderLyrics;
