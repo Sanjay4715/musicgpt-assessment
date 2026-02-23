8a7e6462cf3928280d5325df0a458b4f969c1ab2
Sanjaya Rai <sonjrai@gmail.com>
2026-02-23 11:40:43 +0545

    refactor some code, servery busy event handle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import PromptSection from "@/components/PromptSection/PromptSection";
+import { useCommonStore } from "@/store/useCommonStore";
 import { useLiveGenerationStore } from "@/store/useLiveGenerationStore";
 import { usePresetPromptStore } from "@/store/usePresetPromptStore";
 import { useEffect } from "react";
@@ -8,6 +9,7 @@ import { useEffect } from "react";
 const Home = () => {
   const { getPrompts } = usePresetPromptStore();
   const { submitPrompt, getStatus } = useLiveGenerationStore();
+  const { setStoreState } = useCommonStore();
 
   useEffect(() => {
     getPrompts();
@@ -17,9 +19,14 @@ const Home = () => {
     getStatus();
   }, []);
 
+  const handleSubmitPrompt = (prompt: string) => {
+    submitPrompt(prompt);
+    setStoreState({ isProfileDropdown: true });
+  };
+
   return (
     <div className="w-full min-h-[90vh] px-4 md:px-6 lg:px-8 flex flex-col justify-center">
-      <PromptSection submitPrompt={submitPrompt} />
+      <PromptSection submitPrompt={handleSubmitPrompt} />
     </div>
   );
 };
