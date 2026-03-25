ee72efb288dd88a3767babcce6d7cd2f4f209930
Sanjay Rai <43443174+Sanjay4715@users.noreply.github.com>
2026-03-25 22:22:57 +0545

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -4,7 +4,7 @@ console.log(process.env.NODE_ENV);
 
 const SOCKET_URL =
   process.env.NODE_ENV === "production"
-    ? "https://musicgpt-assessment-socket-production.up.railway.app"
+    ? process.env.SOCKET_URL ?? "https://musicgpt-assessment-socket-production.up.railway.app"
     : "http://localhost:4000/";
 
 export const socket: Socket = io(SOCKET_URL, { autoConnect: true });
